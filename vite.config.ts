825be294742f457cc64f418a01246a0d4ab2ed32
liuzisheng1 <lzs13333464863@163.com>
2024-01-25 15:19:33 +0800

    feat: 设置alias别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
-import { fileURLToPath, URL } from "node:url"
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import vueJsx from "@vitejs/plugin-vue-jsx"
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import AutoImport from "unplugin-auto-import/vite"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import path from "path"
+
+function _resolve(dir: string) {
+  return path.resolve(__dirname, dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,7 +28,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url))
+      "@": _resolve("src")
     }
   },
   server: {
